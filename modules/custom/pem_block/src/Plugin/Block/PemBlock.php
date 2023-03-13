2288bb9e5367b0a9ce0a18521d06e7cd6ae3af22
suria-raul <stadnitzki.nero.nero@gmail.com>
2023-03-13 17:31:52 +0800

    Practiced creating custom block

---
+++ modules/custom/pem_block/src/Plugin/Block/PemBlock.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Drupal\pem_block\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+
+/**
+ * @Block(
+ *   id = "pem_block",
+ *   admin_label = @Translation("Pem Block")
+ * )
+ */
+class PemBlock extends BlockBase {
+  public function build()
+  {
+    return [
+      '#type' => 'markup',
+      '#markup' => $this->t("The Pem Block!"),
+    ];
+  }
+}
