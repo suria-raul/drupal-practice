fe2a8c2ba522a2987becd861acfc22e6c2e17737
suria-raul <stadnitzki.nero.nero@gmail.com>
2023-03-15 00:18:51 +0800

    created custom theme using drush and added gin theme

---
+++ themes/custom/killua/js/killua.js
@@ -0,0 +1,17 @@
+/**
+ * @file
+ * killua behaviors.
+ */
+(function (Drupal) {
+
+  'use strict';
+
+  Drupal.behaviors.killua = {
+    attach: function (context, settings) {
+
+      console.log('It works!');
+
+    }
+  };
+
+} (Drupal));
