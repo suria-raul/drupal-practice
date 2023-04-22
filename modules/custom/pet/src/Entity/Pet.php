9f37eb1ab57de46d70a73d7f85b36fbe1da19982
suria-raul <stadnitzki.nero.nero@gmail.com>
2023-04-22 18:02:42 +0800

    recreated pet.php to identify the problem

--- modules/custom/pet/src/Entity/Pet.php
+++ modules/custom/pet/src/Entity/Pet.php
@@ -3,52 +3,65 @@
 namespace Drupal\pet\Entity;
 
 use Drupal\Core\Entity\ContentEntityBase;
+use Drupal\Core\Entity\EntityStorageInterface;
 use Drupal\Core\Entity\EntityTypeInterface;
 use Drupal\Core\Field\BaseFieldDefinition;
-use Drupal\user\EntityOwnerTrait;
 use Drupal\pet\PetInterface;
+use Drupal\user\EntityOwnerTrait;
 
 /**
+ * Defines the pet entity class.
+ *
  * @ContentEntityType(
  *   id = "pet",
  *   label = @Translation("Pet"),
- *   label_colletion = @Translation("Pets"),
+ *   label_collection = @Translation("Pets"),
+ *   label_singular = @Translation("pet"),
+ *   label_plural = @Translation("pets"),
+ *   label_count = @PluralTranslation(
+ *     singular = "@count pets",
+ *     plural = "@count pets",
+ *   ),
  *   handlers = {
- *    "list_builder" = "Drupal\pet\PetListBuilder",
- *    "views_data" = "Drupal\views\EntityViewsData",
- *    "access" = "Drupal\pet\PetAccessControlHandler",
- *    "form" = {
- *      "add" = "Drupal\pet\Form\PetForm",
- *      "edit" = "Drupal\pet\Form\PetForm",
- *      "delete" = "Drupal\Core\Entity\ContentEntityDeleteForm",
- *    },
- *    "route_provider" = {
+ *     "list_builder" = "Drupal\pet\PetListBuilder",
+ *     "views_data" = "Drupal\views\EntityViewsData",
+ *     "access" = "Drupal\pet\PetAccessControlHandler",
+ *     "form" = {
+ *       "add" = "Drupal\pet\Form\PetForm",
+ *       "edit" = "Drupal\pet\Form\PetForm",
+ *       "delete" = "Drupal\Core\Entity\ContentEntityDeleteForm",
+ *     },
+ *     "route_provider" = {
  *       "html" = "Drupal\Core\Entity\Routing\AdminHtmlRouteProvider",
  *     }
  *   },
  *   base_table = "pet",
- *   administer_permission = "administer pet",
+ *   admin_permission = "administer pet",
  *   entity_keys = {
  *     "id" = "id",
+ *     "label" = "label",
  *     "uuid" = "uuid",
- *     "label" = "name",
- *     "owner" = "uid"
+ *     "owner" = "uid",
  *   },
  *   links = {
- *     "canonical" = "/pet/{pet}",
- *     "collection" = "/admin/content/pets",
+ *     "collection" = "/admin/content/pet",
  *     "add-form" = "/pet/add",
+ *     "canonical" = "/pet/{pet}",
  *     "edit-form" = "/pet/{pet}/edit",
- *     "delete-form" = /pet/{pet}/delete""
+ *     "delete-form" = "/pet/{pet}/delete",
  *   },
- *   field_ui_base_route = "entity.pet.settings"
+ *   field_ui_base_route = "entity.pet.settings",
  * )
  */
 class Pet extends ContentEntityBase implements PetInterface {
 
   use EntityOwnerTrait;
 
+  /**
+   * {@inheritdoc}
+   */
   public static function baseFieldDefinitions(EntityTypeInterface $entity_type) {
+
     $fields = parent::baseFieldDefinitions($entity_type);
 
     $fields['name'] = BaseFieldDefinition::create('string')
@@ -58,26 +71,26 @@ class Pet extends ContentEntityBase implements PetInterface {
       ->setDisplayOptions('form', [
         'type' => 'string_textfield'
       ])
-      ->setDisplayConfigurable('form', true)
+      ->setDisplayConfigurable('form', TRUE)
       ->setDisplayOptions('view', [
         'label' => 'hidden',
         'type' => 'string'
       ])
-      ->setDisplayConfigurable('view', true);
+      ->setDisplayConfigurable('view', TRUE);
 
     $fields['age'] = BaseFieldDefinition::create('integer')
       ->setLabel(t("Age"))
       ->setDescription(t("The age of your pet"))
-      ->setRequired(true)
+      ->setRequired(TRUE)
       ->setDisplayOptions('form', [
         'type' => 'number'
       ])
-      ->setDisplayConfigurable('form', true)
+      ->setDisplayConfigurable('form', TRUE)
       ->setDisplayOptions('view', [
         'label' => 'hidden',
         'type' => 'string'
       ])
-      ->setDisplayConfigurable('view', true);
+      ->setDisplayConfigurable('view', TRUE);
 
     $fields['uid'] = BaseFieldDefinition::create('entity_reference')
       ->setLabel(t('Owner'))
@@ -86,4 +99,5 @@ class Pet extends ContentEntityBase implements PetInterface {
 
     return $fields;
   }
+
 }
