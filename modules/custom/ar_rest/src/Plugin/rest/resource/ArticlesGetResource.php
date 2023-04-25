eb76706a326d05104188cd02b4fdb0ec88825436
suria-raul <stadnitzki.nero.nero@gmail.com>
2023-04-25 20:54:14 +0800

    added Articles get rest resource

---
+++ modules/custom/ar_rest/src/Plugin/rest/resource/ArticlesGetResource.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Drupal\ar_rest\Plugin\rest\resource;
+
+use Drupal\node\Entity\Node;
+use Drupal\rest\Plugin\ResourceBase;
+use Drupal\rest\ResourceResponse;
+
+/**
+ * @RestResource(
+ *   id = "articles_get_resource",
+ *   label = @Translation("Get Articles Resource"),
+ *   uri_paths = {
+ *     "canonical" = "/get-articles"
+ *   }
+ * )
+ */
+class ArticlesGetResource extends ResourceBase{
+
+  public function get() {
+    $nids = \Drupal::entityQuery('node')
+      ->accessCheck(false)
+      ->condition('status', 1)
+      ->condition('type', 'article')
+      ->execute();
+
+    $articles = Node::loadMultiple($nids);
+
+    $response = new ResourceResponse($articles);
+    $response->addCacheableDependency($articles);
+    return $response;
+  }
+}
